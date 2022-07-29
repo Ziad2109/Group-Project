c47adca5130b454ad8c7d08a76cfe18532685609
beeabbas <ummeabeehaabbas@gmail.com>
2022-07-28 19:58:54 -0600

    Changes Made
    Created an FXML file for the application, Josh will work on it to create
    the GUI.
    Add a stream directory through which I will configure the directory.
    Attached the csv file of only Netflix titles.

---
+++ src/application/StreamDirectory.java
@@ -0,0 +1,10 @@
+package application;
+
+public class StreamDirectory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
 module GroupProject {
 	requires javafx.controls;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
