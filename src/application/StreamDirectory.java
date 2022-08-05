ae069e177a94e51012cbbfa2ded2247f3a841193
ummeabeeha.abbas <ummeabeeha.abbas@IC-CS233-39.uc.ucalgary.ca>
2022-08-04 21:41:44 -0600

    Working progress

--- src/application/StreamDirectory.java
+++ src/application/StreamDirectory.java
@@ -1,9 +1,18 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.IOException; 
+import java.nio.charset.StandardCharsets; 
+import java.nio.file.Files; 
+import java.nio.file.Path; 
+import java.nio.file.Paths; 
+import java.util.ArrayList; 
+import java.util.List;
+
 public class StreamDirectory {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Stream> medias = readStreamsFromCSV() 
 
 	}
 
