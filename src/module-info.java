dd2800b469b06dc3b53db9552705459a9891f14a
beeabbas <ummeabeehaabbas@gmail.com>
2022-08-11 15:14:19 -0600

    added requires javafx.graphics line for action event

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module GroupProject {
 	requires javafx.controls;
 	requires javafx.graphics;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
